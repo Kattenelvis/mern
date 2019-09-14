6ce9dac0a0f0bc7b2a989bef93f4af3605b486f5
Kattenelvis <officialkattenelvis@gmail.com>
2019-09-14 21:51:20 +0200

    added React

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
+const items = require("./api/items");
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -11,6 +13,8 @@ const db = require("./config/keys");
 mongoose
   .connect(db.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log(`Server started at ${port}`));
+
+app.use("/api/items", items);
 
 const port = process.env.PORT || 5000;
 app.listen(port);

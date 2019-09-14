1f761d201a8ce2c7fe04b678a00be425bf51f574
Kattenelvis <officialkattenelvis@gmail.com>
2019-09-14 20:55:12 +0200

    initialzied

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://Katten:1234@mycluster-feumr.mongodb.net/test?retryWrites=true&w=majority"
+};
--- server.js
+++ server.js
@@ -5,3 +5,12 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.use(bodyParser.json());
+
+const db = require("./config/keys");
+
+mongoose
+  .connect(db.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log(`Server started at ${port}`));
+
+const port = process.env.PORT || 5000;
+app.listen(port);

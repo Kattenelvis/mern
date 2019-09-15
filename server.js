f3535f553d0a464ed80509874bcb4ba0cd72d4a2
Kattenelvis <officialkattenelvis@gmail.com>
2019-09-15 19:19:52 +0200

    publishing

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const items = require("./api/items");
 
@@ -15,6 +16,13 @@ mongoose
   .then(() => console.log(`Server started at ${port}`));
 
 app.use("/api/items", items);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port);

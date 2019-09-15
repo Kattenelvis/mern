dbb271c6ac68ca705e9bf5564bf3d5869eb23761
Kattenelvis <officialkattenelvis@gmail.com>
2019-09-15 10:11:49 +0200

    added redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import AppNavbar from "./components/AppNavbar";
+import ShoppingList from "./components/ShoppingList";
+
+// import { Provider } from "react-redux";
+// import store from "./redux tutorial/store";
 
 function App() {
-  return <h1>GOOD MORNING VIETNAM</h1>;
+  return (
+    // <Provider store={store}>
+    <div>
+      <AppNavbar />
+      <ShoppingList />
+      <h1>GOOD MORNING VIETNAM</h1>
+    </div>
+    // </Provider>
+  );
 }
 
 export default App;
